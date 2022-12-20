e7f2b31ec2da3d6a315d150e71c5dc21ddd79010
Hưng Lê <bincholon02@gmail.com>
2022-12-20 10:29:32 +0700

    update

---
+++ Core/Inc/global-LeHung.h
@@ -0,0 +1,74 @@
+/*
+ * global.h
+ *
+ *  Created on: Nov 23, 2022
+ *      Author: pmt12
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#define DEFAULT_RED_DURATION			5000
+#define DEFAULT_YELLOW_DURATION			2000
+#define DEFAULT_GREEN_DURATION			3000
+
+#define DIVISION_NUMBER					1000
+
+#define DURATION_1S						1000
+#define DURATION_3S						3000
+
+#define RED_LIGHT						1
+#define YELLOW_LIGHT					2
+#define GREEN_LIGHT						3
+#define PEDESTRIAN_LIGHT_GO				4
+#define PEDESTRIAN_LIGHT_STOP			5
+#define PEDESTRIAN_LIGHT_OFF			6
+
+#define MAX_SEGMENT_VALUE				99
+#define DURATION_FOR_4HZ				250
+#define DURATION_FOR_5HZ				200
+
+#define SEGMENT_IN_A_ROW				2
+#define MAX_LED_SEGMENT					4
+
+#define TIME_FOR_PEDESTRIAN				5000
+
+#define PEDESTRIAN_LIGHT_CYCLE			8
+
+enum TRAFFIC_STATE	{IDLE,
+					AUTO_RED1_GREEN2, AUTO_RED1_YELLOW2, AUTO_GREEN1_RED2, AUTO_YELLOW1_RED2,
+					CHANGE_MODE, CHANGE_MODE_RED, CHANGE_MODE_YELLOW, CHANGE_MODE_GREEN,
+					MODIFY_PRESSED, MODIFY_LONGPRESSED, SET_PRESSED,
+					MANUAL_RED1_GREEN2, MANUAL_RED1_YELLOW2, MANUAL_GREEN1_RED2, MANUAL_YELLOW1_RED2,
+					PEDESTRIAN_STOP, PEDESTRIAN_GO};
+extern enum TRAFFIC_STATE state;
+
+extern int counter_red;
+extern int counter_yellow;
+extern int counter_green;
+extern int counter_pedestrian;
+
+
+extern int buzzer_warning;
+
+extern int isRedMode;
+extern int isYellowMode;
+extern int isGreenMode;
+extern int isInManual;
+extern int isInChange;
+extern int isInAuto;
+extern int isPedestrian;
+extern int isPedestrianPressed;
+
+extern int wasInModify;
+extern int timeCounter;
+extern int tempCounter;
+extern int segment1Counter;
+extern int segment2Counter;
+
+extern int segmentIndexAuto;
+extern int segmentIndexManual;
+
+extern int led_buffer_auto[MAX_LED_SEGMENT];
+extern int led_buffer_manual[MAX_LED_SEGMENT];
+#endif /* INC_GLOBAL_H_ */
---
+++ Core/Src/fsm_automatic-LeHung.c
@@ -0,0 +1,209 @@
+/*
+ * fsm_automatic.c
+ *
+ *  Created on: Nov 23, 2022
+ *      Author: pmt12
+ */
+
+#include "fsm_manual.h"
+#include "fsm_automatic.h"
+#include "fsm_change.h"
+#include "fsm_pedestrian.h"
+#include "global.h"
+#include "button.h"
+#include "software_timer.h"
+#include "led_segment_control.h"
+#include "melody.h"
+#include "main.h"
+
+void trafficLight_automatic() {
+	switch (state) {
+	case IDLE:
+		state = AUTO_RED1_GREEN2;
+		clearTrafficDisplay();
+		segment1Counter = counter_red / DIVISION_NUMBER;
+		segment2Counter = counter_green / DIVISION_NUMBER;
+		setTimerCounter1(DURATION_1S);
+		counter_pedestrian = 0;
+		break;
+	case AUTO_RED1_GREEN2:
+		displayTrafficLight(RED_LIGHT, GREEN_LIGHT);
+		if (isPedestrian == 1)
+			displayPedestrianLight(PEDESTRIAN_LIGHT_GO);
+		if (isPedestrian == 0)
+			displayPedestrianLight(PEDESTRIAN_LIGHT_OFF);
+		if (isInManual == 1) {
+			state = MANUAL_RED1_GREEN2;
+		}
+		if (isPressedAndReleased(BTN_SELECT_INDEX)) {
+			state = MANUAL_RED1_GREEN2;
+			isInManual = 1;
+			isInAuto = 0;
+		}
+		if (is_button_pressed_3s(BTN_SELECT_INDEX)){
+			clearTrafficDisplay();
+			state = CHANGE_MODE;
+			isInChange = 1;
+			setTimerTriggerLed(DURATION_FOR_4HZ);
+		}
+		if (isPressedAndReleased(BTN_PED_INDEX)) {
+			isPedestrian = 1;
+		}
+
+
+		if (countDownTimer1_counter == 1) {
+			segment1Counter--;
+			segment2Counter--;
+
+			if (segment2Counter == 0) {
+				segment2Counter = counter_yellow / DIVISION_NUMBER;
+				state = AUTO_RED1_YELLOW2;
+				//buzzer_warning = 0;
+
+				if (isPedestrian == 1) {
+					counter_pedestrian++;
+					if (counter_pedestrian == PEDESTRIAN_LIGHT_CYCLE) {
+						counter_pedestrian = 0;
+						isPedestrian = 0;
+					}
+				}
+			}
+
+			setTimerCounter1(DURATION_1S);
+		}
+		break;
+	case AUTO_RED1_YELLOW2:
+		displayTrafficLight(RED_LIGHT, YELLOW_LIGHT);
+		if (isPedestrian == 1)
+			displayPedestrianLight(PEDESTRIAN_LIGHT_GO);
+		if (isPedestrian == 0)
+			displayPedestrianLight(PEDESTRIAN_LIGHT_OFF);
+		if (isPressedAndReleased(BTN_SELECT_INDEX)) {
+			isInManual = 1;
+			isInAuto = 0;
+		}
+		if (isPressedAndReleased(BTN_PED_INDEX)) {
+			isPedestrian = 1;
+		}
+
+		if (is_button_pressed_3s(BTN_SELECT_INDEX)){
+			clearTrafficDisplay();
+			state = CHANGE_MODE;
+			isInChange = 1;
+			setTimerTriggerLed(DURATION_FOR_4HZ);
+		}
+		if (countDownTimer1_counter == 1) {
+			segment1Counter--;
+			segment2Counter--;
+			if (segment2Counter <= 0) {
+				segment1Counter = counter_green / DIVISION_NUMBER;
+				segment2Counter = counter_red / DIVISION_NUMBER;
+				state = AUTO_GREEN1_RED2;
+
+				if (isPedestrian == 1) {
+					counter_pedestrian++;
+					if (counter_pedestrian == PEDESTRIAN_LIGHT_CYCLE) {
+						counter_pedestrian = 0;
+						isPedestrian = 0;
+					}
+				}
+			}
+
+			setTimerCounter1(DURATION_1S);
+		}
+		break;
+	case AUTO_GREEN1_RED2:
+		displayTrafficLight(GREEN_LIGHT, RED_LIGHT);
+		if (isPedestrian == 1)
+			displayPedestrianLight(PEDESTRIAN_LIGHT_STOP);
+		if (isPedestrian == 0)
+			displayPedestrianLight(PEDESTRIAN_LIGHT_OFF);
+		if (isInManual == 1) {
+			state = MANUAL_GREEN1_RED2;
+		}
+		if (isPressedAndReleased(BTN_SELECT_INDEX)) {
+			state = MANUAL_GREEN1_RED2;
+			isInManual = 1;
+			isInAuto = 0;
+		}
+		if (is_button_pressed_3s(BTN_SELECT_INDEX)){
+			clearTrafficDisplay();
+			state = CHANGE_MODE;
+			isInChange = 1;
+			setTimerTriggerLed(DURATION_FOR_4HZ);
+		}
+
+		if (isPressedAndReleased(BTN_PED_INDEX)) {
+			isPedestrian = 1;
+		}
+		if (countDownTimer1_counter == 1) {
+			segment1Counter--;
+			segment2Counter--;
+			if (segment1Counter <= 0) {
+				segment1Counter = counter_yellow / DIVISION_NUMBER;
+				state = AUTO_YELLOW1_RED2;
+
+				if (isPedestrian == 1) {
+					counter_pedestrian++;
+					if (counter_pedestrian == PEDESTRIAN_LIGHT_CYCLE) {
+						counter_pedestrian = 0;
+						isPedestrian = 0;
+					}
+				}
+			}
+
+			setTimerCounter1(DURATION_1S);
+		}
+		break;
+	case AUTO_YELLOW1_RED2:
+		displayTrafficLight(YELLOW_LIGHT, RED_LIGHT);
+		if (isPedestrian == 1)
+			displayPedestrianLight(PEDESTRIAN_LIGHT_STOP);
+		if (isPedestrian == 0)
+			displayPedestrianLight(PEDESTRIAN_LIGHT_OFF);
+		if (isPressedAndReleased(BTN_SELECT_INDEX)) {
+			isInManual = 1;
+			isInAuto = 0;
+		}
+		if (is_button_pressed_3s(BTN_SELECT_INDEX)){
+			clearTrafficDisplay();
+			state = CHANGE_MODE;
+			isInChange = 1;
+			setTimerTriggerLed(DURATION_FOR_4HZ);
+		}
+
+		if (isPressedAndReleased(BTN_PED_INDEX)) {
+			isPedestrian = 1;
+		}
+
+		if (isPedestrian == 1) {
+			buzzer_warning = 1;
+		}
+
+		if (countDownTimer1_counter == 1) {
+			segment1Counter--;
+			segment2Counter--;
+			if (segment1Counter <= 0) {
+				segment1Counter = counter_red / DIVISION_NUMBER;
+				segment2Counter = counter_green / DIVISION_NUMBER;
+				state = AUTO_RED1_GREEN2;
+				buzzer_warning = 0;
+
+				if (isPedestrian == 1) {
+					counter_pedestrian++;
+					if (counter_pedestrian == PEDESTRIAN_LIGHT_CYCLE) {
+						counter_pedestrian = 0;
+						isPedestrian = 0;
+					}
+				}
+
+			}
+
+			setTimerCounter1(DURATION_1S);
+		}
+
+		break;
+	default:
+		break;
+	}
+}
